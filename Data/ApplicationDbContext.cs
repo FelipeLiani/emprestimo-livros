0cc81a53be2422acc1fe4ae6539a4f3c0bdb942e
fliani <104031747+FelipeLiani@users.noreply.github.com>
2024-10-21 14:53:44 -0300

    Precisamos criar as migrations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data {
+  public class ApplicationDbContext : DbContext {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+    
+    }
+    public DbSet<EmprestimosModel> Emprestimos {get; set;} // Tabela Emprestismos
+  }
+}
--- Models/EmprestimosModel.cs
+++ Models/EmprestimosModel.cs
@@ -1,10 +1,10 @@
 namespace EmprestimoLivros.Models {
-  public class Emprestimos {
+  public class EmprestimosModel {
     public int Id {get; set;}
     public string Recebedor {get; set;}
     public string Fornecedor {get; set;}
     public string LivroEmprestado {get; set;}
-    public DateTime DataEmprestimo {get; set;} = DateTime.Now;
+    public DateTime DataUltimaAtualizacao {get; set;} = DateTime.Now;
     
   }
 }
